1bab0c09b6eca686dd7a3ae08d42ae03c89e071f
Jash Shah <jash.shah@asint.net>
2026-06-12 15:54:55 +0530

    build ui js

--- build-ui.js
+++ build-ui.js
@@ -1,11 +1,70 @@
-const { execSync } = require("child_process");
+/* eslint-disable */
 
-try {
-    console.log("Building UI...");
-    execSync("ui5 build", { stdio: "inherit" });
+const { exec } = require("child_process");
+const os = require("os");
 
-    console.log("UI build completed");
-} catch (err) {
-    console.error("UI build failed");
-    process.exit(1);
+const buildCMD = "ui5 build preload --clean-dest --config ui5-deploy.yaml --include-task=generateCachebusterInfo";
+const rootFolder = __dirname; // Replace with the root folder If It isn't working
+
+const subfolders = [
+    "asint_ais_cml_config"
+]; // UI folders
+
+const cpus = os.cpus().length;
+const batchSize = Math.max(1, cpus - 1);
+
+const queue = [...subfolders];
+
+let failedToBuild = [];
+let activeProcess = 0;
+
+function execute(cmd, cwd) {
+    return new Promise((resolve, reject) => {
+        exec(cmd, { cwd }, (error, stdout, stderr) => {
+            console.log(stdout);
+            console.error(stderr);
+            error ? reject({ cwd, error }) : resolve(cwd);
+        });
+
+    });
 }
+
+async function processQueue() {
+    while (queue.length > 0 && activeProcess < batchSize) {
+        let folder = queue.shift();
+        let cwd = rootFolder + "/" + folder;
+        
+        activeProcess++;
+
+        execute(buildCMD, cwd)
+            .then(() => {
+                console.log(`Successfully built ${folder}\n\n----------------------------------------------------------------------------------------------`)
+            })
+            .catch((error) => {
+                console.log(`Failed to build ${folder}: ${error}\n\n----------------------------------------------------------------------------------------------`);
+                failedToBuild.push(folder);
+            })
+            .finally(() => {
+                activeProcess--;
+                processQueue();
+            });
+    }
+}
+
+(async function () {
+    console.log("##############################################################################################\n\nStarting UI build\n\n##############################################################################################\n\n");
+
+    for (let i = 0; i < batchSize; i++) {
+        processQueue();
+    }
+
+    while (activeProcess > 0) {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+
+    if (failedToBuild.length > 0) {
+        console.log(`\nThe following modules failed to build:\n${failedToBuild.join(", ")}\n\n##############################################################################################`);
+    } else {
+        console.log(`\nAll modules built successfully\n\n##############################################################################################`);
+    }
+})();
